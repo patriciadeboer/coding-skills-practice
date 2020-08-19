76458df4fc7b4261bf87ab01a170c10615ca80dd
patriciadeboer <pdeboer05@gmail.com>
2020-08-19 16:56:26 -0500

    Add Problem and test

---
+++ test/first-non-repeating.test.js
@@ -0,0 +1,30 @@
+const firstNonRepeatingLetter = require('../lib/first-non-repeating');
+const chai = require("chai");
+const assert = chai.assert;
+
+describe('Simple Tests', function() {
+  it('should handle simple tests', function() {
+    assert.strictEqual(firstNonRepeatingLetter('a'), 'a');
+    assert.strictEqual(firstNonRepeatingLetter('stress'), 't');
+    assert.strictEqual(firstNonRepeatingLetter('moonmen'), 'e');
+  });
+  it('should handle empty strings', function() {
+    assert.strictEqual(firstNonRepeatingLetter(''), '');
+  });
+});
+
+describe('Harder Tests', function() {
+  it('should handle all repeating strings', function() {
+    assert.strictEqual(firstNonRepeatingLetter('abba'), '');
+    assert.strictEqual(firstNonRepeatingLetter('aa'), '');
+  });
+  it('should handle odd characters', function() {
+    assert.strictEqual(firstNonRepeatingLetter('∞§ﬁ›ﬂ∞§'), 'ﬁ');
+    assert.strictEqual(firstNonRepeatingLetter('hello world, eh?'), 'w');
+  });
+  it('should handle letter cases', function() {
+    assert.strictEqual(firstNonRepeatingLetter('sTreSS'), 'T');
+    assert.strictEqual(firstNonRepeatingLetter('Go hang a salami, I\'m a lasagna hog!'), ',');
+  });
+});
+
