cfba7698760c76cdc2496bd37db7243f47c365dc
codingChica <thecodingchica@gmail.com>
2020-08-19 16:06:48 -0500

    Setting up directory and example problem

---
+++ test/closest-multiple-ten.test.js
@@ -0,0 +1,26 @@
+const closestMultiple10 = require('../lib/closest-multiple-ten');
+const chai = require("chai");
+const assert = chai.assert;
+
+describe('closestMultiple10', function() {
+  it('should return 10 for numbers from 10 to 14', function() {
+    for (let i = 10; i <= 14; ++i) {
+      assert.strictEqual(closestMultiple10(i), 10);
+    }
+  });
+
+  it('should return 20 for numbers from 15 to 20', function() {
+    for (let i = 15; i <= 20; ++i) {
+      assert.strictEqual(closestMultiple10(i), 20);
+    }
+  });
+});
+
+describe('random tests', function() {
+  const solution = num => Math.round(num / 10) * 10;
+  it('should show correct result', function() {
+    for (let i = 1337; i < 13337; i += 333 + (Math.random() * 10)) {
+      assert.strictEqual(closestMultiple10(i), solution(i));
+    }
+  });
+});
