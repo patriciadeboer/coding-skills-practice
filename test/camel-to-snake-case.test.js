84d3dbca46d8fd0df8fd3b67dc79afa4ea7c5bdd
Patricia de Boer <pdeboer05@gmail.com>
2020-08-25 22:05:54 -0500

    Add problem camelCase to snake_case and related tests

---
+++ test/camel-to-snake-case.test.js
@@ -0,0 +1,30 @@
+const toUnderscore = require('../lib/camel-to-snake-case');
+const chai = require("chai");
+const assert = chai.assert;
+
+describe('Convert CamelCase to snake_case', ()=> {
+  describe('basic tests', function() {
+    it('should equals test_controller for string TestController', function() {
+      assert.equal(toUnderscore('TestController'), 'test_controller')
+    });
+
+    it('should equals this_is_beautiful_day for string ThisIsBeautifulDay', function() {
+      assert.equal(toUnderscore('ThisIsBeautifulDay'), 'this_is_beautiful_day')
+    });
+
+  });
+
+  describe('tests with numbers', function() {
+    it('should equals am7_days for string Am7Days', function() {
+      assert.equal(toUnderscore('Am7Days'), 'am7_days')
+    });
+
+    it('should equals my3_code_is4_times_better for string My3CodeIs4TimesBetter', function() {
+      assert.equal(toUnderscore('My3CodeIs4TimesBetter'), 'my3_code_is4_times_better')
+    });
+
+    it("should equals '5' for 5", function() {
+      assert.equal(toUnderscore(5), '5')
+    });
+  });
+});
